44377dd6a7a11fb2e9b6772fd90ce4a39a3df56c
yassine lahbib <yassine.lahbib@esprit.tn>
2021-12-01 14:21:04 +0100

    Updates

---
+++ precache-manifest.5bc1b638e3f0e91efd8305d792da3b97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22677b3caa2938203c7b34536faaf304",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "1061e3129f67a19cf12a",
+    "url": "/cv/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "13ee809069e70835154f",
+    "url": "/cv/static/css/main.bb368cb6.chunk.css"
+  },
+  {
+    "revision": "1061e3129f67a19cf12a",
+    "url": "/cv/static/js/2.b262abbc.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/cv/static/js/2.b262abbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ee809069e70835154f",
+    "url": "/cv/static/js/main.515f0823.chunk.js"
+  },
+  {
+    "revision": "4084e6dcb6b5541507a4",
+    "url": "/cv/static/js/runtime-main.fdce2138.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
